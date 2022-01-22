fb481dada9383e5e553e63146e49a00a6667eb0e
Aswin <aswin2000.ak@gmail.com>
2022-01-22 20:35:32 +0530

    Added MultiLayerPerceptron class with placeholder functions.

---
+++ multi_layer_perceptron/mlp.py
@@ -0,0 +1,22 @@
+import pickle
+import os
+
+class MultiLayerPerceptron:
+    def __init__(self):
+        pass
+
+
+    def train_model(self):
+        pass
+
+
+    def predict(self):
+        pass
+
+
+    def load_model(self):
+        pass
+
+
+    def save_model(self):
+        pass
